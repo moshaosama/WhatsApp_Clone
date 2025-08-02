b39537560265323b2b37a4e8bc0d8f5a91b43298
moshaosama <mohamedOSFekry@gmail.com>
2025-08-02 16:46:00 +0300

    Add HeaderChat component

---
+++ src/Features/Chats/Components/HeaderChat.tsx
@@ -0,0 +1,26 @@
+import { HiMiniEllipsisVertical } from "react-icons/hi2";
+import { IoSearch } from "react-icons/io5";
+
+const HeaderChat = () => {
+  return (
+    <>
+      <div className="bg-[#141414] flex justify-between items-center text-white w-[80pc] p-4">
+        <div className="flex gap-4 items-center">
+          <img
+            src="/96368389-e1cd-47d4-8e5f-3ac1084f3a83.jpeg"
+            alt="userImg.png"
+            className="w-12 rounded-full h-12 object-cover"
+          />
+          <h1 className="font-bold">Mosha</h1>
+        </div>
+
+        <div className="flex gap-6">
+          <IoSearch size={25} />
+          <HiMiniEllipsisVertical size={25} />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default HeaderChat;
