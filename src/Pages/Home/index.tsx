9bacfa82585a71cb47f35b37d9e143da11412c56
moshaosama <mohamedOSFekry@gmail.com>
2025-08-02 16:45:54 +0300

    Update Home page index

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,12 +1,21 @@
-import Header from "../../Features/Chats/Components/Header";
-import User from "../../Features/Chats/Components/User";
+import Chat from "../../Features/Chats/Components/Chat";
+import HeaderChat from "../../Features/Chats/Components/HeaderChat";
+import Header from "../../Features/Users/Components/Header";
+import User from "../../Features/Users/Components/User";
 
 const Home = () => {
   return (
     <>
-      <div className="bg-[#141414] text-white border-r-[0.5px] w-[35pc] p-4 border-[#3d3d3d]">
-        <Header />
-        <User />
+      <div className="flex w-full">
+        <div className="bg-[#141414] text-white border-r-[0.5px] w-[35pc] p-4 border-[#3d3d3d]">
+          <Header />
+          <User />
+        </div>
+
+        <div>
+          <HeaderChat />
+          <Chat />
+        </div>
       </div>
     </>
   );
