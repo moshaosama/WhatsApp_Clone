7def4d9aa98c72462741c3fbbc6f0da09bcaa346
moshaosama <mohamedOSFekry@gmail.com>
2025-08-02 16:45:53 +0300

    Update Chat component

--- src/Features/Chats/Components/Chat.tsx
+++ src/Features/Chats/Components/Chat.tsx
@@ -1,13 +1,22 @@
-
-
 const Chat = () => {
   return (
     <>
-        <div>
-            
-        </div>
-    </>
-  )
-}
+      <div className="relative">
+        <img
+          src="/ts7vuoswhwf41.jpg"
+          alt="chatImage.png"
+          className="object-cover h-[91vh] w-full"
+        />
 
-export default Chat
+        <form className="absolute bottom-3 left-5 w-[77.5pc]">
+          <input
+            type="text"
+            className="bg-[#303030] px-2 text-white w-full py-3 rounded-full"
+          />
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default Chat;
