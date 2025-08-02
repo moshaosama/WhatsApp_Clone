1a739a7748cbdf597bbd1e740ac206df2cb8de90
moshaosama <mohamedOSFekry@gmail.com>
2025-08-03 01:21:19 +0300

    Update Chat component

--- src/Features/Chats/Components/Chat.tsx
+++ src/Features/Chats/Components/Chat.tsx
@@ -1,6 +1,13 @@
 import { IoSend } from "react-icons/io5";
+import useSendMessage from "../Hooks/useSendMessage";
+import cn from "../../../Utils/cn";
 
 const Chat = () => {
+  const { handleSendMessage, handleSubmit, register, Message, sendMessage } =
+    useSendMessage();
+
+  console.log(Message, sendMessage);
+
   return (
     <>
       <div className="relative">
@@ -10,10 +17,32 @@ const Chat = () => {
           className="object-cover h-[91vh] w-full"
         />
 
-        <form className="absolute bottom-3 left-5 w-[77.5pc]">
+        <div className={cn("flex flex-col absolute top-4  w-full gap-3")}>
+          {Message && (
+            <div className="w-[76pc] flex  justify-start">
+              <div className="text-white rounded-xl  p-3 bg-[#2c2c2c]">
+                <h1 className="text-xl font-bold">{Message}</h1>
+              </div>
+            </div>
+          )}
+
+          {sendMessage && (
+            <div className="w-full flex justify-end">
+              <div className="text-white text-end  rounded-xl p-3 bg-green-600">
+                <h1 className="text-xl font-bold">{sendMessage?.message}</h1>
+              </div>
+            </div>
+          )}
+        </div>
+
+        <form
+          className="absolute bottom-3 left-5 w-[77.5pc]"
+          onSubmit={handleSubmit(handleSendMessage)}
+        >
           <input
             type="text"
             className="bg-[#303030] px-2 text-white w-full py-3 rounded-full"
+            {...register("message", { required: true })}
           />
 
           <p className="absolute bottom-2 right-3">
