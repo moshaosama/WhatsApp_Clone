dc5c2105a0ea3b6ade97281aa213b208a0f2d55f
moshaosama <mohamedOSFekry@gmail.com>
2025-08-03 01:21:20 +0300

    Update Home page index

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,9 +1,12 @@
+import DownloadApp from "../../Components/DownloadApp";
 import Chat from "../../Features/Chats/Components/Chat";
 import HeaderChat from "../../Features/Chats/Components/HeaderChat";
 import Header from "../../Features/Users/Components/Header";
 import User from "../../Features/Users/Components/User";
+import useGetUsers from "../../Features/Users/Hooks/useGetusers";
 
 const Home = () => {
+  const { UserById } = useGetUsers();
   return (
     <>
       <div className="flex w-full">
@@ -12,10 +15,14 @@ const Home = () => {
           <User />
         </div>
 
-        <div>
-          <HeaderChat />
-          <Chat />
-        </div>
+        {UserById === null ? (
+          <DownloadApp />
+        ) : (
+          <div>
+            <HeaderChat />
+            <Chat />
+          </div>
+        )}
       </div>
     </>
   );
